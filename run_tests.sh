600e6c4a07e2eae78df0615be0f852d38cfb2865
Petr Tichy <petr@pex.com>
2020-12-01 13:41:21 +0000

    Update run_tests.sh
    - automate pulling of generated certificate

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export AE_SERVICE_ADDRESS=localhost:6060
-#export AE_SERVICE_ROOTS_FILE=/home/stepan/Pex/ae-sdk/data/crt
-export GRPC_DEFAULT_SSL_ROOTS_FILE_PATH=/home/stepan/Pex/ae-sdk/data/crt
+
+curl -k -o $PWD/ca.crt "https://$AE_SERVICE_ADDRESS/roots"
+export AE_SERVICE_ROOTS_FILE=$PWD/ca.crt
 
 GRPC_VERBOSITY=debug go test
