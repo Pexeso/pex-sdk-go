bc50ac74dda5c7e86fbce5874a0145152a263411
Stepan Bujnak <stepan.bujnak@gmail.com>
2021-12-07 16:35:34 +0100

    check version compatibility with the native library

---
+++ 000init.go
@@ -0,0 +1,20 @@
+package pexae
+
+// This file is deliberately named like this so that it's first compiled and
+// first executed because it performs version checks to make sure these
+// bindings are compatible with the native library.
+
+// #cgo pkg-config: pexae
+//
+// #define AE_SDK_MAJOR_VERSION 2
+// #define AE_SDK_MINOR_VERSION 0
+//
+// #include <pex/ae/sdk/version.h>
+import "C"
+
+func init() {
+	compatible := C.AE_Version_IsCompatible(C.AE_SDK_MAJOR_VERSION, C.AE_SDK_MINOR_VERSION)
+	if !compatible {
+		panic("bindings are not compatible with the native library")
+	}
+}
