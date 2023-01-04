c0840068c610928f90d242e4098a9e6d135c63d1
Jon Anderson <jon.david.anderson@gmail.com>
2023-01-03 22:18:37 -0800

    Choose which fingerprint types to create.

--- fingerprint.go
+++ fingerprint.go
@@ -8,6 +8,16 @@ package pexae
 import "C"
 import "unsafe"
 
+// FingerprintType is a bit flag specifying one or more fingerprint types.
+type FingerprintType int
+
+const (
+	Video  FingerprintType = 1
+	Audio  FingerprintType = 2
+	Melody FingerprintType = 4
+	All    FingerprintType = Video | Audio | Melody
+)
+
 // Fingerprint is how the SDK identifies a piece of digital content.
 // It can be generated from a media file or from a memory buffer. The
 // content must be encoded in one of the supported formats and must be
@@ -23,19 +33,21 @@ func NewFingerprint(b []byte) *Fingerprint {
 }
 
 // FingerprintFile is used to generate a fingerprint from a
-// file stored on a disk. The parameter to the function must be a path
-// to a valid file in supported format.
-func (x *Client) FingerprintFile(path string) (*Fingerprint, error) {
-	return newFingerprint([]byte(path), true)
+// file stored on a disk. The path parameter must be a path
+// to a valid file in supported format. The typ parameter
+// specifies which types of fingerprints to create.
+func (x *Client) FingerprintFile(path string, typ FingerprintType) (*Fingerprint, error) {
+	return newFingerprint([]byte(path), true, typ)
 }
 
 // FingerprintBuffer is used to generate a fingerprint from a
-// media file loaded in memory as a byte slice.
-func (x *Client) FingerprintBuffer(buffer []byte) (*Fingerprint, error) {
-	return newFingerprint(buffer, false)
+// media file loaded in memory as a byte slice. The typ parameter
+// specifies which types of fingerprints to create.
+func (x *Client) FingerprintBuffer(buffer []byte, typ FingerprintType) (*Fingerprint, error) {
+	return newFingerprint(buffer, false, typ)
 }
 
-func newFingerprint(input []byte, isFile bool) (*Fingerprint, error) {
+func newFingerprint(input []byte, isFile bool, typ FingerprintType) (*Fingerprint, error) {
 	C.AE_Lock()
 	defer C.AE_Unlock()
 
@@ -54,7 +66,7 @@ func newFingerprint(input []byte, isFile bool) (*Fingerprint, error) {
 		cFile := C.CString(string(input))
 		defer C.free(unsafe.Pointer(cFile))
 
-		C.AE_Fingerprint_File(cFile, ft, status, C.AE_Fingerprint_Type_All)
+		C.AE_Fingerprint_File(cFile, ft, status, C.int(typ))
 	} else {
 		buf := C.AE_Buffer_New()
 		if buf == nil {
@@ -66,7 +78,7 @@ func newFingerprint(input []byte, isFile bool) (*Fingerprint, error) {
 		size := C.size_t(len(input))
 
 		C.AE_Buffer_Set(buf, data, size)
-		C.AE_Fingerprint_Buffer(buf, ft, status, C.AE_Fingerprint_Type_All)
+		C.AE_Fingerprint_Buffer(buf, ft, status, C.int(typ))
 	}
 
 	if err := statusToError(status); err != nil {
