8b555b151d582f701e84b7fa70776c2bc06f7b8b
Martin Hořeňovský <martin.horenovsky@gmail.com>
2023-07-22 21:11:04 +0200

    Support multiple lookup IDs in search

--- pex_search.go
+++ pex_search.go
@@ -23,7 +23,7 @@ type PexSearchRequest struct {
 // completion.
 type PexSearchResult struct {
 	// An ID that uniquely identifies a particular search. Can be used for diagnostics.
-	LookupID string
+	LookupIDs []string
 
 	// The assets which the query matched against.
 	Matches []*PexSearchMatch
@@ -72,7 +72,7 @@ type PexSearchMatch struct {
 type PexSearchFuture struct {
 	client *PexSearchClient
 
-	LookupID string
+	LookupIDs []string
 }
 
 // Get blocks until the search result is ready and then returns it. It
@@ -100,10 +100,12 @@ func (x *PexSearchFuture) Get() (*PexSearchResult, error) {
 	}
 	defer C.AE_CheckSearchResult_Delete(&cResult)
 
-	cLookupID := C.CString(x.LookupID)
-	defer C.free(unsafe.Pointer(cLookupID))
 
-	C.AE_CheckSearchRequest_SetLookupID(cRequest, cLookupID)
+	for _, lookupID := range x.LookupIDs {
+		cLookupID := C.CString(lookupID)
+		defer C.free(unsafe.Pointer(cLookupID))
+		C.AE_CheckSearchRequest_AddLookupID(cRequest, cLookupID)
+	}
 
 	C.AE_CheckSearch(x.client.c, cRequest, cResult, cStatus)
 	if err := statusToError(cStatus); err != nil {
@@ -159,7 +161,7 @@ func (x *PexSearchFuture) processResult(cResult *C.AE_CheckSearchResult, cStatus
 	}
 
 	return &PexSearchResult{
-		LookupID: x.LookupID,
+		LookupIDs: x.LookupIDs,
 		Matches:  matches,
 	}, nil
 }
@@ -240,8 +242,16 @@ func (x *PexSearchClient) StartSearch(req *PexSearchRequest) (*PexSearchFuture, 
 		return nil, err
 	}
 
+	var cLookupIDPos C.size_t = 0
+	var lookupIDs []string
+	var cLookupID *C.char
+
+	for C.AE_StartSearchResult_NextLookupID(cResult, &cLookupIDPos, &cLookupID) {
+		lookupIDs = append(lookupIDs, C.GoString(cLookupID))
+	}
+
 	return &PexSearchFuture{
 		client:   x,
-		LookupID: C.GoString(C.AE_StartSearchResult_GetLookupID(cResult)),
+		LookupIDs: lookupIDs,
 	}, nil
 }
