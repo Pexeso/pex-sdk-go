f066ea4a0eb88e93c9533906b2707f385f9cf1c7
Stepan Bujnak <stepan.bujnak@gmail.com>
2023-08-12 13:08:37 +0200

    remove license and metadata searches

--- client.go
+++ client.go
@@ -6,37 +6,9 @@ package pexae
 // #include <pex/ae/sdk/init.h>
 // #include <pex/ae/sdk/lock.h>
 // #include <pex/ae/sdk/client.h>
-// #include <pex/ae/sdk/license_search.h>
-// #include <pex/ae/sdk/metadata_search.h>
 // #include <stdlib.h>
 import "C"
 import "unsafe"
-
-// Client serves as an entry point to all operations that
-// communicate with the Attribution Engine backend service. It
-// automatically handles the connection and authentication with the
-// service.
-type Client struct {
-	fingerprinter
-
-	c *C.AE_Client
-}
-
-// NewClient initializes connections and authenticates with the
-// backend service with the credentials provided as arguments.
-func NewClient(clientID, clientSecret string) (*Client, error) {
-	cClient, err := newClient(C.AE_LICENSE_SEARCH, clientID, clientSecret)
-	if err != nil {
-		return nil, err
-	}
-	return &Client{
-		c: cClient,
-	}, nil
-}
-
-func (x *Client) getCClient() *C.AE_Client {
-	return x.c
-}
 
 func newClient(typ C.AE_ClientType, clientID, clientSecret string) (*C.AE_Client, error) {
 	cClientID := C.CString(clientID)
@@ -71,19 +43,13 @@ func newClient(typ C.AE_ClientType, clientID, clientSecret string) (*C.AE_Client
 		panic("out of memory")
 	}
 
-	C.AE_Client_InitType(cClient, typ, cClientID, cClientSecret, cStatus)
+	C.AE_Client_Init(cClient, typ, cClientID, cClientSecret, cStatus)
 	if err := statusToError(cStatus); err != nil {
 		// TODO: if this fails, run AE_Cleanup
 		C.free(unsafe.Pointer(cClient))
 		return nil, err
 	}
 	return cClient, nil
-}
-
-// Close closes all connections to the backend service and releases
-// the memory manually allocated by the core library.
-func (x *Client) Close() error {
-	return closeClient(&x.c)
 }
 
 func closeClient(c **C.AE_Client) error {
