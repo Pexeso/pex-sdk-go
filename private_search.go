7ef3aa4e2641f193f3428f914fe2a67ee2f70cfb
Stepan Bujnak <stepan.bujnak@gmail.com>
2023-10-25 16:52:36 +0200

    remove printf statement

--- private_search.go
+++ private_search.go
@@ -96,8 +96,6 @@ func (x *PrivateSearchFuture) processResult(cResult *C.Pex_CheckSearchResult, cS
 	cJSON := C.Pex_CheckSearchResult_GetJSON(cResult)
 	j := C.GoString(cJSON)
 
-	fmt.Println("JSON OUTPUT:", j)
-
 	res := new(PrivateSearchResult)
 	if err := json.Unmarshal([]byte(j), res); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal result: %w", err)
