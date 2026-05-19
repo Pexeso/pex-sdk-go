ec0a324f0dac2f0aaa38689133f7e4f394ef4c04
Stepan Bujnak <stepan.bujnak@gmail.com>
2026-05-19 12:47:00 +0200

    added PrivateSearchClient.Unarchive() function

--- private_search.go
+++ private_search.go
@@ -261,6 +261,26 @@ func (x *PrivateSearchClient) Archive(id string, types ...FingerprintType) error
 	return statusToError(cStatus)
 }
 
+// Unarchive unarchives a fingerprint that was previously archived in the private search
+// catalog. The catalog is determined from the authentication credentials used
+// when initializing the client.
+func (x *PrivateSearchClient) Unarchive(id string, types ...FingerprintType) error {
+	C.Pex_Lock()
+	defer C.Pex_Unlock()
+
+	cStatus := C.Pex_Status_New()
+	if cStatus == nil {
+		panic("out of memory")
+	}
+	defer C.Pex_Status_Delete(&cStatus)
+
+	cID := C.CString(id)
+	defer C.free(unsafe.Pointer(cID))
+
+	C.Pex_Unarchive(x.c, cID, C.int(reduceTypes(types)), cStatus)
+	return statusToError(cStatus)
+}
+
 type EntryStatus int
 
 func (x *EntryStatus) UnmarshalJSON(data []byte) error {
